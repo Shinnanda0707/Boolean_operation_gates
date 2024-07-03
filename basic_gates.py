f632e838924d2f3055bbbef91817262cc40d35ad
Shinnanda0707 <choihyun0723@naver.com>
2024-07-03 16:11:13 +0900

    new file:   basic_gates.py 	modified:   main.py

---
+++ basic_gates.py
@@ -0,0 +1,13 @@
+# Declare AND gate
+def and_gate(input_1: bool, input_2: bool) -> bool:
+    return input_1 and input_2
+
+
+# Declare OR gate
+def or_gate(input_1: bool, input_2: bool) -> bool:
+    return input_1 or input_2
+
+
+# Declare NOT gate
+def not_gate(input_1: bool) -> bool:
+    return not input_1
--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from basic_gates import or_gate, and_gate, not_gate
 
+
+# Declare XOR gate
+def xor_gate(input_1: bool, input_2: bool) -> bool:
+    # Set variable x to (input_1 or input_2)
+    x = or_gate(input_1, input_2)
+
+    # Set variable y to not(input_1 and input_2)
+    y = and_gate(input_1, input_2) # y = input_1 and input_2
+    y = not_gate(y) # y = not(input_1 and input_2)
+
+    # Calculate the result
+    result = or_gate(x, y)
+
+    return result
+
+
+# Input a, b and print a XOR b
+a = bool(input("Input a (0 or 1): "))
+b = bool(input("Input b (0 or 1): "))
+print(f"a XOR b = {xor_gate(a, b)}")
